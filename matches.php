90f371a993c6c98c1202575b37c99bdeaf7773fd
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 17:10:57 -0500

    Update matches.php

--- matches.php
+++ matches.php
@@ -7,7 +7,7 @@ include "view-header.php";
 
 $competitionId = isset($_GET['id']) ? $_GET['id'] : null;
 $matchesData = selectMatchesByCompetition($competitionId); // Call the correct function name
-include "matches.php";
+include "view-matches.php";
 
 include "view-footer.php";
 ?>
