b7ae6c981a52d5890d73a95ed1bfb0edf314ec9d
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 12:42:34 -0500

    Create model-matches.php

---
+++ model-matches.php
@@ -0,0 +1,29 @@
+<?php
+require_once("util-db.php");
+
+function getMatchesByCompetition($competitionId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
+                               FROM matches m 
+                               JOIN teams t1 ON m.team1_id = t1.team_id 
+                               JOIN teams t2 ON m.team2_id = t2.team_id 
+                               WHERE m.competition_id = ?");
+        $stmt->bind_param("i", $competitionId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $matchesData = array();
+        while ($row = $result->fetch_assoc()) {
+            $matchesData[] = $row;
+        }
+
+        $stmt->close();
+        $conn->close();
+        return $matchesData;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
