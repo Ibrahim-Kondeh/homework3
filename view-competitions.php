2461ddf797682a0b0c918118eaf45beedc74f13b
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 12:15:27 -0500

    Update view-competitions.php

--- view-competitions.php
+++ view-competitions.php
@@ -2,40 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Competitions</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        /* Custom CSS for the competitions table */
-        .competition-table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .competition-table th, .competition-table td {
-            padding: 12px;
-            border: 1px solid #ddd;
-            text-align: left;
-        }
-
-        .competition-table th {
-            background-color: #f2f2f2;
-        }
-
-        /* Apply striped rows for better readability */
-        .competition-table tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-        .competition-table tr:hover {
-            background-color: #ff0000; /* Change background color on hover */
-        }
-
-        .highlight-row {
-            transition: background-color 0.3s; /* Smooth transition effect for row highlight */
-        }
-    </style>
+    <!-- ... (head content remains the same) ... -->
 </head>
 
 <body>
@@ -47,6 +14,7 @@
                     <th>Competition Name</th>
                     <th>Start Date</th>
                     <th>End Date</th>
+                    <th>Action</th> <!-- Added a new column for the button -->
                 </tr>
             </thead>
             <tbody>
@@ -55,8 +23,11 @@
                 ?>
                     <tr class="highlight-row">
                         <td><?php echo $competition['competition_name']; ?></td>
-                        <td><?php echo $competition['satart_date']; ?></td>
+                        <td><?php echo $competition['start_date']; ?></td>
                         <td><?php echo $competition['end_date']; ?></td>
+                        <td>
+                            <a href="matches.php?competition=<?php echo $competition['competition_name']; ?>" class="btn btn-primary">View Matches</a>
+                        </td>
                     </tr>
                 <?php
                 }
