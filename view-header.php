73714f3cf62021657fa19b14aebca718918fa190
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-21 23:06:19 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,27 +1,15 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        .navbar {
-            background-color: #4CAF50; /* Green */
-        }
-
-        .navbar-brand, .nav-link {
-            color: white;
-        }
-
-        .navbar-toggler-icon {
-            background-color: white;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file -->
 </head>
 
 <body>
+
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
