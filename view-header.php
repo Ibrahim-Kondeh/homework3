77adc93a0207478ae91949e1b54c58738eab5742
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 00:30:07 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -27,31 +27,33 @@
         .highlight-row:hover {
             background-color: #ff0000;
         }
-
-        /* Custom CSS for navigation bar */
-        .navbar {
-            background-color: #4CAF50;
-        }
-
-        .navbar-brand, .nav-link {
-            color: white;
-            padding: 15px 20px;
-            text-decoration: none;
-        }
-
-        .navbar-brand:hover, .nav-link:hover {
-            background-color: #3e8e41;
-        }
     </style>
 </head>
 
 <body>
-    <nav class="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="/">Home</a>
-        <a class="nav-link" href="teams.php">Teams</a>
-        <a class="nav-link" href="players.php">Players</a>
-        <a class="nav-link" href="matches.php">Matches</a>
-        <a class="nav-link" href="competitions.php">Competitions</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Tables
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <?php
+                        $tablePages = array("teams.php", "players.php", "matches.php", "competitions.php");
+                        $tableNames = array("Teams", "Players", "Matches", "Competitions");
+                        for ($i = 0; $i < count($tablePages); $i++) {
+                            echo '<li><a class="dropdown-item" href="' . $tablePages[$i] . '">' . $tableNames[$i] . '</a></li>';
+                        }
+                        ?>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </nav>
 
     <h1>Teams</h1>
