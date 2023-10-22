8c3406fe538a9b97434715fa849d465bbececfca
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 13:27:37 -0500

    Update model-matches.php

--- model-matches.php
+++ model-matches.php
@@ -1,29 +1,20 @@
 <?php
-require_once("util-db.php");
-
 function getMatchesByCompetition($competitionId) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
-                               FROM matches m 
-                               JOIN teams t1 ON m.team1_id = t1.team_id 
-                               JOIN teams t2 ON m.team2_id = t2.team_id 
-                               WHERE m.competition_id = ?");
-        $stmt->bind_param("i", $competitionId);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        $matchesData = array();
-        while ($row = $result->fetch_assoc()) {
-            $matchesData[] = $row;
-        }
-
-        $stmt->close();
-        $conn->close();
-        return $matchesData;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
+    global $mysqli;
+    $query = "SELECT c.competition_name, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2
+              FROM competition c
+              JOIN matches m ON c.competition_id = m.competition_id
+              JOIN teams t1 ON m.team1_id = t1.team_id
+              JOIN teams t2 ON m.team2_id = t2.team_id
+              WHERE c.competition_id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $competitionId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $matchesData = array();
+    while ($row = $result->fetch_assoc()) {
+        $matchesData[] = $row;
     }
+    return $matchesData;
 }
 ?>
