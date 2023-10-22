21daa75717238d14fe46d3e5472782232b6cf0d7
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 00:23:07 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,69 +1,34 @@
 <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        /* Custom CSS to style the teams table */
-        .team-table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .team-table th, .team-table td {
-            padding: 10px;
-            border: 1px solid #ddd;
-            text-align: left;
-        }
-
-        .team-table th {
-            background-color: #f2f2f2;
-        }
-
-        /* Custom CSS for table row highlighting */
-        .highlight-row:hover {
-            background-color: #ff0000;
-        }
-    </style>
-</head>
-
-<body>
-    <h1>Teams</h1>
-    <table class="team-table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Team Name</th>
-                <th>Coach</th>
-                <th>Founded Year</th>
-                <th>Home Stadium</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $rowNumber = 0;
-            while ($team = $teams->fetch_assoc()) {
-                $rowNumber++;
-            ?>
-                <tr class="highlight-row">
-                    <td><?php echo $team['team_id']; ?></td>
-                    <td><?php echo $team['team_name']; ?></td>
-                    <td><?php echo $team['coach_name']; ?></td>
-                    <td><?php echo $team['founded_year']; ?></td>
-                    <td><?php echo $team['home_stadium']; ?></td>
-                    <td><a href="players.php?id=<?php echo $team['team_id']; ?>">Players</a></td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
-</body>
-
-</html>
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="member.php">Member</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="books.php">Books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="books-borrowed-by-member.php">Borrowed Books</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
