13317ecefe0d9e046c48be93b13911f514b379aa
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 12:20:11 -0500

    Update view-competitions.php

--- view-competitions.php
+++ view-competitions.php
@@ -2,7 +2,46 @@
 <html lang="en">
 
 <head>
-    <!-- ... (head content remains the same) ... -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Competitions</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        /* Custom CSS for the competitions table */
+        .competition-table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .competition-table th,
+        .competition-table td {
+            padding: 12px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+
+        .competition-table th {
+            background-color: #f2f2f2;
+        }
+
+        /* Apply striped rows for better readability */
+        .competition-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .competition-table tr:hover {
+            background-color: #ff0000;
+        }
+
+        .highlight-row {
+            transition: background-color 0.3s;
+        }
+
+        .view-matches-btn {
+            font-size: 18px; /* Set font size */
+            padding: 12px 20px; /* Set padding */
+        }
+    </style>
 </head>
 
 <body>
@@ -26,7 +65,7 @@
                         <td><?php echo $competition['satart_date']; ?></td>
                         <td><?php echo $competition['end_date']; ?></td>
                         <td>
-                            <a href="matches.php?competition=<?php echo $competition['competition_name']; ?>" class="btn btn-primary">View Matches</a>
+                            <a href="matches.php?competition=<?php echo $competition['competition_name']; ?>" class="btn btn-primary view-matches-btn">View Matches</a>
                         </td>
                     </tr>
                 <?php
