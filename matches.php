9265f04bc44f331977fc1f7010fd1c28cc8b8f3e
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 12:58:11 -0500

    Update matches.php

--- matches.php
+++ matches.php
@@ -2,7 +2,7 @@
 require_once("model-matches.php");
 
 // Assuming you have $competitionId from somewhere, for example, $_GET['id']
-$competitionId = $_GET['id'];
+$competition_id = $_GET['id'];
 $matchesData = getMatchesByCompetition($competitionId);
 include("view-matches.php");
 ?>
