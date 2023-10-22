3d562bf11e7f4ee52dd3bb75d44ef157ceed756f
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-21 22:59:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,28 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>HomeWork 3</h1>
- <?php
+
+<div class="container">
+    <h1>Football for Homework 3 </h1>
+    <!-- Table content and other page elements go here -->
+
+    <div class="card">
+        <h2>League Logos</h2>
+        <div>
+            <img src="premier-league-logo.png" alt="Premier League Logo">
+        </div>
+        <div>
+            <img src="champions-league-logo.png" alt="Champions League Logo">
+        </div>
+        <div>
+            <img src="community-shield-logo.png" alt="Community Shield Logo">
+        </div>
+        <div>
+            <img src="efl-logo.png" alt="EFL Logo">
+        </div>
+    </div>
+</div>
+
+<?php
 include "view-footer.php";
 ?>
