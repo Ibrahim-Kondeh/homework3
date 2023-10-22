60c62e91c512e92d82036297d39df0ff2dadfab3
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 14:42:32 -0500

    Update matches.php

--- matches.php
+++ matches.php
@@ -5,7 +5,7 @@ require_once("model-matches.php");
 $pageTitle = "Matches";
 include "view-header.php";
 
-$matchesData = getAllMatchesGroupedByCompetition(); // Assuming you have a function to get matches grouped by competition
+$matchesData = selectMatchesByCompetition(); // Assuming you have a function to get matches grouped by competition
 include "view-matches-approach1.php";
 
 include "view-footer.php";
