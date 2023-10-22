55be76178cf95d48a129792f560d4ff1c4a864e2
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-21 23:19:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
-<?php
-$pageTitle = "Home";
-include "view-header.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="container">
-    <h1>Football for Homework 3 </h1>
-    <!-- Table content and other page elements go here -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file -->
+</head>
 
-    <div class="card">
-        <h2>The Beautiful Game</h2>
-        <div>
-            <img src="premier-league-logo.png" alt="Premier League Logo">
-        </div>
-        <div>
-            <img src="champions-league-logo.png" alt="Champions League Logo">
-        </div>
-        <div>
-            <img src="community-shield-logo.png" alt="Community Shield Logo">
-        </div>
-        <div>
-            <img src="efl-logo.png" alt="EFL Logo">
-        </div>
-    </div>
+<body>
+
+<div class="navbar">
+    <a href="index.php">Home</a>
+    <a href="teams.php">Teams</a>
+    <!-- Add more links for other pages if needed -->
 </div>
 
-<?php
-include "view-footer.php";
-?>
+<div class="container">
+    <h1>HomeWork 3</h1>
+
+    <div class="card">
+        <h2>English Premier League</h2>
+        <img src="premier-league-logo.png" alt="Premier League Logo">
+    </div>
+
+    <div class="card">
+        <h2>Champions League</h2>
+        <img src="champions-league-logo.png" alt="Champions League Logo">
+    </div>
+
+    <div class="card">
+        <h2>Community Shield</h2>
+        <img src="community-shield-logo.png" alt="Community Shield Logo">
+    </div>
+
+    <div class="card">
+        <h2>EFL</h2>
+        <img src="efl-logo.png" alt="EFL Logo">
+    </div>
+
+</div>
+
+</body>
+
+</html>
