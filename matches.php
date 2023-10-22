ab104be622f6fb96dbc10cdadf96d3e665550603
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 13:17:51 -0500

    Update matches.php

--- matches.php
+++ matches.php
@@ -5,9 +5,9 @@ require_once("model-matches.php");
 $pageTitle = "Matches";
 include "view-header.php";
 
-// Check if 'id' parameter is set in the URL
-if(isset($_GET['id'])) {
-    $competitionId = $_GET['id'];
+// Check if 'competition_id' parameter is set in the URL
+if(isset($_GET['competition_id'])) {
+    $competitionId = $_GET['competition_id']; // Assuming 'competition_id' is the parameter name
     $matchesData = getMatchesByCompetition($competitionId);
     include "view-matches.php";
 } else {
