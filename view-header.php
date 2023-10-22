7404f69b135072568d46ea0582cda1b245362422
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 00:31:36 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         /* Custom CSS to style the teams table */
         .team-table {
@@ -27,41 +26,42 @@
         .highlight-row:hover {
             background-color: #ff0000;
         }
+
+        /* Custom CSS for horizontal navigation bar */
+        .navbar {
+            background-color: #4CAF50;
+            overflow: hidden;
+        }
+
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #3e8e41;
+        }
     </style>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Home</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Tables
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php
-                        $tablePages = array("teams.php", "players.php", "matches.php", "competitions.php");
-                        $tableNames = array("Teams", "Players", "Matches", "Competitions");
-                        for ($i = 0; $i < count($tablePages); $i++) {
-                            echo '<li><a class="dropdown-item" href="' . $tablePages[$i] . '">' . $tableNames[$i] . '</a></li>';
-                        }
-                        ?>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <div class="navbar">
+        <a href="/">Home</a>
+        <a href="teams.php">Teams</a>
+        <a href="players.php">Players</a>
+        <a href="matches.php">Matches</a>
+        <a href="competitions.php">Competitions</a>
+    </div>
 
     <h1>Teams</h1>
     <table class="team-table">
         <!-- Table content goes here -->
     </table>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
 </body>
 
 </html>
