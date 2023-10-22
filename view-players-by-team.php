b28ac006fc03bb6b195fa685c0489ed75886662d
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 11:34:58 -0500

    Update view-players-by-team.php

--- view-players-by-team.php
+++ view-players-by-team.php
@@ -3,6 +3,36 @@
 
 <head>
     <!-- ... (head content remains the same) ... -->
+    <style>
+        /* Custom CSS for the wider table and striped rows */
+        .player-table {
+            width: 100%; /* Set the table width to 100% */
+            border-collapse: collapse;
+        }
+
+        .player-table th, .player-table td {
+            padding: 12px; /* Increase padding for better spacing */
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+
+        .player-table th {
+            background-color: #f2f2f2;
+        }
+
+        /* Apply striped rows for better readability */
+        .player-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        .player-table tr:hover {
+            background-color: #ff0000; /* Change background color on hover */
+        }
+
+        .highlight-row {
+            transition: background-color 0.3s; /* Smooth transition effect for row highlight */
+        }
+    </style>
 </head>
 
 <body>
