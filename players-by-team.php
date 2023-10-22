4d501d4cc4bc6f880e08b1b5e3c87ae173021c3d
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 11:29:48 -0500

    Update players-by-team.php

--- players-by-team.php
+++ players-by-team.php
@@ -8,7 +8,7 @@ include "view-header.php";
 // Check if 'id' parameter is set in the URL
 if(isset($_GET['id'])) {
     $teamId = $_GET['id'];
-    $players = selectPlayersByTeam($team_id);
+    $players = selectPlayersByTeam($teamId); // Corrected variable name
     include "view-players-by-team.php";
 } else {
     echo "Team ID not provided.";
