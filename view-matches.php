320273105c974dfaa65f493b73889f02d04b4318
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 14:17:03 -0500

    Update view-matches.php

--- view-matches.php
+++ view-matches.php
@@ -10,18 +10,22 @@ include "view-header.php";
     $currentCompetition = "";
     foreach ($matchesData as $match) {
         if ($match['competition_name'] != $currentCompetition) {
+            if ($currentCompetition != "") {
+                echo "</tbody></table>"; // Close the previous table if it's not the first one
+            }
             echo "<h2>" . htmlspecialchars($match['competition_name']) . "</h2>";
             echo "<table class='table'>";
-            echo "<thead><tr><th>Date</th><th>Match</th><th>Score</th></tr></thead>";
+            echo "<thead><tr><th>Team 1</th><th>Team 2</th><th>Score</th></tr></thead>";
             echo "<tbody>";
             $currentCompetition = $match['competition_name'];
         }
         echo "<tr>";
-        echo "<td>" . htmlspecialchars($match['match_date']) . "</td>";
-        echo "<td>" . htmlspecialchars($match['team1_name']) . " vs " . htmlspecialchars($match['team2_name']) . "</td>";
+        echo "<td>" . htmlspecialchars($match['team1_name']) . "</td>";
+        echo "<td>" . htmlspecialchars($match['team2_name']) . "</td>";
         echo "<td>" . htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']) . "</td>";
         echo "</tr>";
     }
+    echo "</tbody></table>"; // Close the last table
     ?>
 </div>
 
