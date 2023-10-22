b580f75624440e9d0f6f24aeecb9cb7d7c840392
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-21 22:21:47 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', 'ikonoucr_homeW3user', '9XLRhtvr!!!!!!', 'ikonoucr_homework3');
+    $conn = new mysqli('159.89.47.44', 'ikonoucr_homework3user', '9XLRhtvr!!!!!!', 'ikonoucr_homework3');
     
     // Check connection
     if ($conn->connect_error) {
