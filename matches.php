4a6e70364b79380322d6509f7a635fc3ca65b823
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 13:28:45 -0500

    Update matches.php

--- matches.php
+++ matches.php
@@ -1,18 +1,9 @@
 <?php
 require_once("util-db.php");
-require_once("model-matches.php");
+require_once("model.php");
 
-$pageTitle = "Matches";
-include "view-header.php";
-
-// Check if 'competition_id' parameter is set in the URL
-if(isset($_GET['competition_id'])) {
-    $competitionId = $_GET['competition_id']; // Assuming 'competition_id' is the parameter name
-    $matchesData = getMatchesByCompetition($competitionId);
-    include "view-matches.php";
-} else {
-    echo "Competition ID not provided.";
-}
-
-include "view-footer.php";
+// Assuming you have $competitionId from somewhere, for example, $_GET['id']
+$competitionId = $_GET['id'];
+$matchesData = getMatchesByCompetition($competitionId);
+include("view-matches.php");
 ?>
