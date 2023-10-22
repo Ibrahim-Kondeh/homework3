8709892737547d6150b36de0d04ef8003b0edd85
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-21 21:22:02 -0500

    Create model-teams.php

---
+++ model-teams.php
@@ -0,0 +1,16 @@
+<?php
+function selectTeams() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name, founded_year, home_stadium FROM `teams`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    } 
+}
+
+?>
