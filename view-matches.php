2a5baeade2487223dba0b5f0cdf04a71e2ab9f4b
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 13:26:58 -0500

    Update view-matches.php

--- view-matches.php
+++ view-matches.php
@@ -1,33 +1,28 @@
-<!doctype html>
-<html lang="en">
+<?php
+$pageTitle = "Matches";
+include "view-header.php";
+?>
 
-<head>
-    <!-- ... (head content remains the same) ... -->
-</head>
+<div class="container">
+    <h1>Matches</h1>
 
-<body>
-    <div class="container">
-        <h1>Matches</h1>
-
-        <?php
-        foreach ($matchesData as $match) {
-        ?>
-            <div class="card my-4">
-                <div class="card-header">
-                    <?php echo "Match ID: " . $match['match_id']; ?>
-                </div>
-                <div class="card-body">
-                    <p>Date: <?php echo $match['match_date']; ?></p>
-                    <p>Team 1: <?php echo $match['team1_name']; ?>, Score: <?php echo $match['score_team1']; ?></p>
-                    <p>Team 2: <?php echo $match['team2_name']; ?>, Score: <?php echo $match['score_team2']; ?></p>
-                </div>
-            </div>
-        <?php
+    <?php
+    $currentCompetition = "";
+    foreach ($matchesData as $match) {
+        if ($match['competition_name'] != $currentCompetition) {
+            echo "<h2>" . htmlspecialchars($match['competition_name']) . "</h2>";
+            echo "<table class='table'>";
+            echo "<thead><tr><th>Date</th><th>Match</th><th>Score</th></tr></thead>";
+            echo "<tbody>";
+            $currentCompetition = $match['competition_name'];
         }
-        ?>
-    </div>
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($match['match_date']) . "</td>";
+        echo "<td>" . htmlspecialchars($match['team1_name']) . " vs " . htmlspecialchars($match['team2_name']) . "</td>";
+        echo "<td>" . htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']) . "</td>";
+        echo "</tr>";
+    }
+    ?>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include "view-footer.php"; ?>
