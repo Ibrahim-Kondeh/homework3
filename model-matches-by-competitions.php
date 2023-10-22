2c4a691f0ea3adcae03bb27b460e1ead12da6e5e
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-22 14:34:56 -0500

    Update model-matches-by-competitions.php

--- model-matches-by-competitions.php
+++ model-matches-by-competitions.php
@@ -1,34 +1,24 @@
 <?php
 require_once("util-db.php");
 
-function getAllMatchesGroupedByCompetition() {
+function selectMatchesByCompetition($competitionId) {
     try {
         $conn = get_db_connection();
-        $query = "SELECT c.competition_name, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date
-                  FROM matches m
-                  JOIN teams t1 ON m.team1_id = t1.team_id
-                  JOIN teams t2 ON m.team2_id = t2.team_id
-                  JOIN competition c ON m.competition_id = c.competition_id
-                  ORDER BY c.competition_name, m.match_date";
-        $result = $conn->query($query);
+        $stmt = $conn->prepare("SELECT m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
+                               FROM matches m 
+                               JOIN teams t1 ON m.team1_id = t1.team_id 
+                               JOIN teams t2 ON m.team2_id = t2.team_id 
+                               WHERE m.competition_id = ?");
+        $stmt->bind_param("i", $competitionId);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
         $matchesData = array();
         while ($row = $result->fetch_assoc()) {
-            $competitionName = $row['competition_name'];
-            $match = array(
-                'team1_name' => $row['team1_name'],
-                'team2_name' => $row['team2_name'],
-                'match_date' => $row['match_date']
-            );
-
-            if (!isset($matchesData[$competitionName])) {
-                $matchesData[$competitionName] = array();
-            }
-
-            $matchesData[$competitionName][] = $match;
+            $matchesData[] = $row;
         }
 
-        $result->close();
+        $stmt->close();
         $conn->close();
         return $matchesData;
     } catch (Exception $e) {
