dc768d7e813bbea4fc87111e35a4f6e7f9c41394
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 00:07:45 -0500

    Update matches.php

--- matches.php
+++ matches.php
@@ -5,8 +5,7 @@ require_once("model-matches.php");
 $pageTitle = "Matches";
 include "view-header.php";
 
-$competitionId = isset($_GET['id']) ? $_GET['id'] : null;
-$matchesData = selectMatchesByCompetition($competitionId);
+$matchesData = getAllMatchesGroupedByCompetition();
 include "view-matches.php";
 
 include "view-footer.php";
