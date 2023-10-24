bd7ecb931a35cdde256499bb846ca9869eb60a12
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 01:55:25 -0500

    Update model-matches-by-competitions.php

--- model-matches-by-competitions.php
+++ model-matches-by-competitions.php
@@ -1,29 +1,27 @@
 <?php
 require_once("util-db.php");
 
-function selectMatchesByCompetition($competitionId) {
+function getMatchesByCompetition($competitionName) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
-                               FROM matches m 
-                               JOIN teams t1 ON m.team1_id = t1.team_id 
-                               JOIN teams t2 ON m.team2_id = t2.team_id 
-                               WHERE m.competition_id = ?");
-        $stmt->bind_param("i", $competitionId);
+        $stmt = $conn->prepare("
+            SELECT m.match_id, m.match_date, m.score_team1, m.score_team2,
+                   t1.team_name AS team1_name, t2.team_name AS team2_name
+            FROM matches m
+            JOIN competition c ON m.competition_id = c.competition_id
+            JOIN teams t1 ON m.team1_id = t1.team_id
+            JOIN teams t2 ON m.team2_id = t2.team_id
+            WHERE c.competition_name = ?
+        ");
+        $stmt->bind_param("s", $competitionName);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        $matchesData = array();
-        while ($row = $result->fetch_assoc()) {
-            $matchesData[] = $row;
-        }
-
-        $stmt->close();
-        $conn->close();
-        return $matchesData;
+        // Process the result and return it
+        return $result;
     } catch (Exception $e) {
-        // Handle the exception or log the error if needed
-        throw $e;
+        // Handle exceptions
+        return null;
     }
 }
 ?>
