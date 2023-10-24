97b7dae7b8e2bf546d7e6e2602daf90702280fa1
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 02:31:09 -0500

    Update view-matches-by-competitions.php

--- view-matches-by-competitions.php
+++ view-matches-by-competitions.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  
+    <!-- ... (head content remains the same) ... -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         /* Increase the table size */
@@ -16,11 +16,6 @@
 <body>
     <div class="container">
         <h1>Matches for Competition: <?php echo $competitionName; ?></h1>
-        <!-- Button Group -->
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
-            <button class="btn btn-primary me-md-2" type="button">Button 1</button>
-            <button class="btn btn-primary" type="button">Button 2</button>
-        </div>
         <div class="table-responsive">
             <table class="matches-table table">
                 <thead>
