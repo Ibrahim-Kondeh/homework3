693bc320a3eafd676a63408a77a37795f2c44d28
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 02:28:20 -0500

    Update view-matches-by-competitions.php

--- view-matches-by-competitions.php
+++ view-matches-by-competitions.php
@@ -2,7 +2,8 @@
 <html lang="en">
 
 <head>
-    <!-- ... (head content remains the same) ... -->
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         /* Increase the table size */
         .matches-table th,
@@ -15,8 +16,11 @@
 <body>
     <div class="container">
         <h1>Matches for Competition: <?php echo $competitionName; ?></h1>
-        <!-- Add Bootstrap button -->
-        <a href="#" class="btn btn-primary mb-3">View Matches</a>
+        <!-- Button Group -->
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
+            <button class="btn btn-primary me-md-2" type="button">Button 1</button>
+            <button class="btn btn-primary" type="button">Button 2</button>
+        </div>
         <div class="table-responsive">
             <table class="matches-table table">
                 <thead>
@@ -25,30 +29,27 @@
                         <th>Match Date</th>
                         <th>Home Team</th>
                         <th>Away Team</th>
-                        <th>Score Home Team</th>
-                        <th>Score Away Team</th>
+                        <th>Score (Home: Away)</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    while ($match = $matches->fetch_assoc()) {
-                    ?>
-                        <tr>
-                            <td><?php echo $match['match_id']; ?></td>
-                            <td><?php echo $match['match_date']; ?></td>
-                            <td><?php echo $match['team1_name']; ?></td>
-                            <td><?php echo $match['team2_name']; ?></td>
-                            <td><?php echo $match['score_team1']; ?></td>
-                            <td><?php echo $match['score_team2']; ?></td>
-                        </tr>
-                    <?php
+                    foreach ($matches as $match) {
+                        echo "<tr>";
+                        echo "<td>{$match['match_id']}</td>";
+                        echo "<td>{$match['match_date']}</td>";
+                        echo "<td>{$match['team1_name']}</td>";
+                        echo "<td>{$match['team2_name']}</td>";
+                        echo "<td>{$match['score_team1']} : {$match['score_team2']}</td>";
+                        echo "</tr>";
                     }
                     ?>
                 </tbody>
             </table>
         </div>
     </div>
-    <!-- ... (Bootstrap JS and other scripts) ... -->
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
 </body>
 
 </html>
