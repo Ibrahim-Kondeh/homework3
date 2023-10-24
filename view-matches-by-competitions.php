d110426a9a5f91dbe0eff13800672cbc645f9b46
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 02:22:52 -0500

    Update view-matches-by-competitions.php

--- view-matches-by-competitions.php
+++ view-matches-by-competitions.php
@@ -2,37 +2,53 @@
 <html lang="en">
 
 <head>
- 
+    <!-- ... (head content remains the same) ... -->
+    <style>
+        /* Increase the table size */
+        .matches-table th,
+        .matches-table td {
+            padding: 20px;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Matches</h1>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Match ID</th>
-                    <th>Match Date</th>
-                    <th>Home Team</th>
-                    <th>Away Team</th>
-                    <th>Score</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                while ($match = $matches->fetch_assoc()) {
-                    echo "<tr>";
-                    echo "<td>{$match['match_id']}</td>";
-                    echo "<td>{$match['match_date']}</td>";
-                    echo "<td>{$match['team1_name']}</td>";
-                    echo "<td>{$match['team2_name']}</td>";
-                    echo "<td>{$match['score_team1']} - {$match['score_team2']}</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+    <div class="container">
+        <h1>Matches for Competition: <?php echo $competitionName; ?></h1>
+        <!-- Add Bootstrap button -->
+        <a href="#" class="btn btn-primary mb-3">View Matches</a>
+        <div class="table-responsive">
+            <table class="matches-table table">
+                <thead>
+                    <tr>
+                        <th>Match ID</th>
+                        <th>Match Date</th>
+                        <th>Home Team</th>
+                        <th>Away Team</th>
+                        <th>Score Home Team</th>
+                        <th>Score Away Team</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($match = $matches->fetch_assoc()) {
+                    ?>
+                        <tr>
+                            <td><?php echo $match['match_id']; ?></td>
+                            <td><?php echo $match['match_date']; ?></td>
+                            <td><?php echo $match['team1_name']; ?></td>
+                            <td><?php echo $match['team2_name']; ?></td>
+                            <td><?php echo $match['score_team1']; ?></td>
+                            <td><?php echo $match['score_team2']; ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
+    <!-- ... (Bootstrap JS and other scripts) ... -->
 </body>
 
 </html>
