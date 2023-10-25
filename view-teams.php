2f162ead41b4dd05ee221a5696ece03606713621
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 00:25:35 -0500

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -2,32 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        /* Custom CSS to style the teams table */
-        .team-table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .team-table th, .team-table td {
-            padding: 10px;
-            border: 1px solid #ddd;
-            text-align: left;
-        }
-
-        .team-table th {
-            background-color: #f2f2f2;
-        }
-
-        /* Custom CSS for table row highlighting */
-        .highlight-row:hover {
-            background-color: #ff0000;
-        }
-    </style>
+    <!-- ... (head content remains the same) ... -->
 </head>
 
 <body>
@@ -44,6 +19,8 @@
                             <th>Founded Year</th>
                             <th>Home Stadium</th>
                             <th></th>
+                            <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -58,7 +35,17 @@
                                 <td><?php echo $team['coach_name']; ?></td>
                                 <td><?php echo $team['founded_year']; ?></td>
                                 <td><?php echo $team['home_stadium']; ?></td>
-                                <td><a href="players-by-team.php?id=<?php echo $team['team_id']; ?>">Players</a></td>
+                                <td>
+                                    <a href="edit-team.php?id=<?php echo $team['team_id']; ?>" class="btn btn-primary btn-sm">
+                                        <i class="bi bi-pencil-fill"></i> Edit
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="delete-team.php?id=<?php echo $team['team_id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this team?')">
+                                        <i class="bi bi-trash-fill"></i> Delete
+                                    </a>
+                                </td>
+                                <td><a href="players-by-team.php?id=<?php echo $team['team_id']; ?>" class="btn btn-info btn-sm">Players</a></td>
                             </tr>
                         <?php
                         }
@@ -67,15 +54,13 @@
                 </table>
             </div>
             <div class="col-md-3">
-                
-
                 <!-- Include the form within the modal -->
                 <?php include "view-teams-newform.php"; ?>
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
+    <!-- ... (rest of the body content remains the same) ... -->
 </body>
 
 </html>
