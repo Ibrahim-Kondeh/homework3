7edbc8153bb9478e2a14c3e9f40430d7cb14325c
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 00:03:10 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -1,16 +1,23 @@
 <?php
 require_once("util-db.php");
 require_once("model-teams.php");
-
 $pageTitle = "Teams";
 include "view-header.php";
 
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            insertTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear'], $_POST['tStadium']);
+            // Attempt to insert data into the database
+            $insertSuccess = insertTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear'], $_POST['tStadium']);
+            
+            if ($insertSuccess) {
+                // If insertion is successful, set a session variable
+                $_SESSION['team_added'] = true;
+            } else {
+                // Handle insertion failure if needed
+            }
             break;
-        // Add more cases if you have other actions
+        // Add more cases if necessary for other actions
     }
 }
 
