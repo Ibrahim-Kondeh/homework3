c24d233786125f97fdb27cfe2e11253903dd7e0f
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 00:08:19 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -1,27 +1,31 @@
 <?php
 require_once("util-db.php");
 require_once("model-teams.php");
+
 $pageTitle = "Teams";
 include "view-header.php";
+
+$successMessage = "";
 
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            // Attempt to insert data into the database
-            $insertSuccess = insertTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear'], $_POST['tStadium']);
-            
-            if ($insertSuccess) {
-                // If insertion is successful, set a session variable
-                $_SESSION['team_added'] = true;
+            if (insertTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear'], $_POST['tStadium'])) {
+                $successMessage = "Team added successfully!";
             } else {
-                // Handle insertion failure if needed
+                $successMessage = "Failed to add team. Please try again.";
             }
             break;
-        // Add more cases if necessary for other actions
+
     }
 }
 
 $teams = selectTeams();
 include "view-teams.php";
+
+if (!empty($successMessage)) {
+    echo '<div class="alert alert-success" role="alert">' . $successMessage . '</div>';
+}
+
 include "view-footer.php";
 ?>
