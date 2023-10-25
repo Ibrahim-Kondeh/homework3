23c6a365ae52886a78526bb6c9e839a0f97905b9
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 23:20:47 -0500

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -31,37 +31,47 @@
 </head>
 
 <body>
-    <h1>Teams</h1>
-    <table class="team-table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Team Name</th>
-                <th>Coach</th>
-                <th>Founded Year</th>
-                <th>Home Stadium</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $rowNumber = 0;
-            while ($team = $teams->fetch_assoc()) {
-                $rowNumber++;
-            ?>
-                <tr class="highlight-row">
-                    <td><?php echo $team['team_id']; ?></td>
-                    <td><?php echo $team['team_name']; ?></td>
-                    <td><?php echo $team['coach_name']; ?></td>
-                    <td><?php echo $team['founded_year']; ?></td>
-                    <td><?php echo $team['home_stadium']; ?></td>
-                    <td><a href="players-by-team.php?id=<?php echo $team['team_id']; ?>">Players</a></td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <h1>Teams</h1>
+                <table class="team-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Team Name</th>
+                            <th>Coach</th>
+                            <th>Founded Year</th>
+                            <th>Home Stadium</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $rowNumber = 0;
+                        while ($team = $teams->fetch_assoc()) {
+                            $rowNumber++;
+                        ?>
+                            <tr class="highlight-row">
+                                <td><?php echo $team['team_id']; ?></td>
+                                <td><?php echo $team['team_name']; ?></td>
+                                <td><?php echo $team['coach_name']; ?></td>
+                                <td><?php echo $team['founded_year']; ?></td>
+                                <td><?php echo $team['home_stadium']; ?></td>
+                                <td><a href="players-by-team.php?id=<?php echo $team['team_id']; ?>">Players</a></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-3">
+                <!-- Include the form here -->
+                <?php include "view-teams-newform.php"; ?>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
 </body>
