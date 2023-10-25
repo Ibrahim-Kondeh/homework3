f7ef0332f30254a9fe736ab368941042c00b70b3
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 23:49:38 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -3,6 +3,13 @@ require_once("util-db.php");
 require_once("model-teams.php");
 $pageTitle = "Teams";
 include "view-header.php";
+if (isset($_POST['actionType'])) {
+switch ($_POST['actionType'])) {
+  case "Add":
+  insertTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear']; $_POST['tStadium']);
+  break;
+ }
+}
 $teams = selectTeams();
 include "view-teams.php";
 include "view-footer.php";
