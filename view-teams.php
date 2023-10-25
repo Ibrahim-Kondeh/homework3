c64da8961a8825688488c48b288c6277411238cd
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 00:48:05 -0500

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -96,12 +96,7 @@
         </div>
     </div>
 
-    <!-- Add button at the top right corner -->
-    <div class="fixed-top-button">
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newTeamModal">
-            <i class="bi bi-plus-circle"></i> Add Team
-        </button>
-    </div>
+   
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
 </body>
