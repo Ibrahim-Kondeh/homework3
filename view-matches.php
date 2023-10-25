e054026ab7151fea5f51063d5d2a0f2269f65569
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 11:19:28 -0500

    Update view-matches.php

--- view-matches.php
+++ view-matches.php
@@ -16,7 +16,32 @@ $pageTitle = "Matches";
         width: 100%;
         padding: 20px;
         display: flex;
-        flex-wrap: wrap;
+        flex-wrap: wrap;<?php
+
+
+function getAllMatchesGroupedByCompetition() {
+    try {
+        $conn = get_db_connection();
+        $query = "SELECT c.competition_name, m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
+                  FROM matches m 
+                  JOIN competition c ON m.competition_id = c.competition_id
+                  JOIN teams t1 ON m.team1_id = t1.team_id 
+                  JOIN teams t2 ON m.team2_id = t2.team_id 
+                  ORDER BY c.competition_name, m.match_date";
+        $result = $conn->query($query);
+        $matchesData = array();
+
+        while ($row = $result->fetch_assoc()) {
+            $matchesData[$row['competition_name']][] = $row;
+        }
+
+        $conn->close();
+        return $matchesData;
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+?>
         justify-content: center;
     }
 
