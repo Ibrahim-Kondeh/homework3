b6195357e9b37f3f0e7b471f102d4de883709e55
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-24 23:10:36 -0500

    Update view-matches.php

--- view-matches.php
+++ view-matches.php
@@ -1,26 +1,81 @@
 <?php
 $pageTitle = "Matches";
-include "view-header.php";
+
 ?>
 
-<div class="container">
-    <h1>Matches</h1>
+<style>
+    body {
+        background-color: #333; /* Set the background color */
+        color: white;
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
 
+    .container {
+        width: 100%;
+        padding: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .table-container {
+        background-color: rgba(0, 0, 0, 0.7);
+        margin: 20px;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    .table-container table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 10px;
+    }
+
+    .table-container th, .table-container td {
+        padding: 10px;
+        text-align: center;
+        border: 1px solid #ddd;
+    }
+
+    .table-container th {
+        background-color: #444;
+    }
+
+    .table-container tr:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+</style>
+
+<div class="container">
     <?php
     foreach ($matchesData as $competition => $matches) {
-        echo "<h2>" . htmlspecialchars($competition) . "</h2>";
-        echo "<table class='table'>";
-        echo "<thead><tr><th>Teams</th><th>Date</th></tr></thead>";
-        echo "<tbody>";
-
-        foreach ($matches as $match) {
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($match['team1_name']) . " vs " . htmlspecialchars($match['team2_name']) . "</td>";
-            echo "<td>" . htmlspecialchars($match['match_date']) . "</td>";
-            echo "</tr>";
-        }
-
-        echo "</tbody></table>";
+    ?>
+        <div class="table-container">
+            <h2><?php echo htmlspecialchars($competition); ?></h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Home Team</th>
+                        <th>Away Team</th>
+                        <th>Date</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($matches as $match) { ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($match['team1_name']); ?></td>
+                            <td><?php echo htmlspecialchars($match['team2_name']); ?></td>
+                            <td><?php echo htmlspecialchars($match['match_date']); ?></td>
+                            <td><?php echo htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']); ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    <?php
     }
     ?>
 </div>
