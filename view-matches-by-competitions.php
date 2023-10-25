fbc385091280be8238459e6709e9d997c48b6cba
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 11:00:02 -0500

    Update view-matches-by-competitions.php

--- view-matches-by-competitions.php
+++ view-matches-by-competitions.php
@@ -1,30 +1,50 @@
-<?php
-$pageTitle = "Matches by Competition";
-include "view-header.php";
-?>
+<!doctype html>
+<html lang="en">
 
-<div class="container">
-    <h1>Matches by Competition</h1>
-
-    <?php
-    foreach ($matchesData as $competition => $matches) {
-        echo "<h2>" . htmlspecialchars($competition) . "</h2>";
-        echo "<table class='table'>";
-        echo "<thead><tr><th>Teams</th><th>Date</th><th>Score</th></tr></thead>";
-        echo "<tbody>";
-
-        foreach ($matches as $match) {
-            $score = htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']);
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($match['team1_name']) . " vs " . htmlspecialchars($match['team2_name']) . "</td>";
-            echo "<td>" . htmlspecialchars($match['match_date']) . "</td>";
-            echo "<td>" . $score . "</td>";
-            echo "</tr>";
+<head>
+    <!-- ... (head content remains the same) ... -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        /* Increase the table size */
+        .matches-table th,
+        .matches-table td {
+            padding: 20px;
         }
+    </style>
+</head>
 
-        echo "</tbody></table>";
-    }
-    ?>
-</div>
+<body>
+    <div class="container">
+        <h1>Matches for Competition: <?php echo $competitionName; ?></h1>
+        <div class="table-responsive">
+            <table class="matches-table table">
+                <thead>
+                    <tr>
+                        <th>Match ID</th>
+                        <th>Match Date</th>
+                        <th>Home Team</th>
+                        <th>Away Team</th>
+                        <th>Score (Home: Away)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($matches as $match) {
+                        echo "<tr>";
+                        echo "<td>{$match['match_id']}</td>";
+                        echo "<td>{$match['match_date']}</td>";
+                        echo "<td>{$match['team1_name']}</td>";
+                        echo "<td>{$match['team2_name']}</td>";
+                        echo "<td>{$match['score_team1']} : {$match['score_team2']}</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
+</body>
 
-<?php include "view-footer.php"; ?>
+</html>
