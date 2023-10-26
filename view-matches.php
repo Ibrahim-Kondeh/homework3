70ae7102eff40e290b6d00e432e9a4b24fc2926e
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-25 23:57:30 -0500

    Update view-matches.php

--- view-matches.php
+++ view-matches.php
@@ -1,119 +1,133 @@
 <?php
 $pageTitle = "Matches";
+include "view-header.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<style>
+    body {
+        background-color: #333; /* Set the background color */
+        color: white;
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $pageTitle; ?></title>
-    <!-- Bootstrap CSS and Bootstrap Icons CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap-icons.css" rel="stylesheet">
-    <style>
-        body {
-            background-color: #333;
-            color: white;
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
+    .container {
+        width: 100%;
+        padding: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 
-        .container {
-            width: 100%;
-            padding: 20px;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-        }
+    .table-container {
+        background-color: rgba(0, 0, 0, 0.7);
+        margin: 20px;
+        padding: 20px;
+        border-radius: 10px;
+    }
 
-        .table-container {
-            background-color: rgba(0, 0, 0, 0.7);
-            margin: 20px;
-            padding: 20px;
-            border-radius: 10px;
-        }
+    .table-container table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 10px;
+    }
 
-        .table-container table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 10px;
-        }
+    .table-container th,
+    .table-container td {
+        padding: 10px;
+        text-align: center;
+        border: 1px solid #ddd;
+    }
 
-        .table-container th,
-        .table-container td {
-            padding: 10px;
-            text-align: center;
-            border: 1px solid #ddd;
-        }
+    .table-container th {
+        background-color: #444;
+    }
 
-        .table-container th {
-            background-color: #444;
-        }
+    .table-container tr:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+</style>
 
-        .table-container tr:hover {
-            background-color: rgba(255, 255, 255, 0.1);
-        }
-
-        .add-match-button {
-            text-align: right;
-            margin-bottom: 20px;
-        }
-    </style>
-</head>
-
-<body>
-    <?php include "view-header.php"; ?>
-
-    <div class="container">
-        <div class="add-match-button">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addMatchModal">
+<div class="container">
+    <?php
+    foreach ($matchesData as $competition => $matches) {
+    ?>
+        <div class="table-container">
+            <h2><?php echo htmlspecialchars($competition); ?></h2>
+            <!-- Add Match Button -->
+            <button type="button" class="btn btn-primary btn-sm mb-3" data-bs-toggle="modal" data-bs-target="#addMatchModal<?php echo htmlspecialchars($competition); ?>">
                 Add Match
             </button>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Home Team</th>
+                        <th>Away Team</th>
+                        <th>Date</th>
+                        <th>Score</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($matches as $match) { ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($match['team1_name']); ?></td>
+                            <td><?php echo htmlspecialchars($match['team2_name']); ?></td>
+                            <td><?php echo htmlspecialchars($match['match_date']); ?></td>
+                            <td><?php echo htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']); ?></td>
+                            <td>
+                                <!-- Edit Match Icon -->
+                                <button class="btn btn-secondary btn-sm edit-match-icon" data-bs-toggle="modal" data-bs-target="#editMatchModal<?php echo $match['match_id']; ?>">Edit</button>
+                                <!-- Delete Match Icon -->
+                                <button class="btn btn-danger btn-sm delete-match-icon" data-bs-toggle="modal" data-bs-target="#deleteMatchModal<?php echo $match['match_id']; ?>">Delete</button>
+                            </td>
+                        </tr>
+
+                        <!-- Edit Match Modal for Each Match -->
+                        <div class="modal fade" id="editMatchModal<?php echo $match['match_id']; ?>" tabindex="-1" aria-labelledby="editMatchModalLabel<?php echo $match['match_id']; ?>" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <!-- Edit Match Modal Content Goes Here -->
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Delete Match Modal for Each Match -->
+                        <div class="modal fade" id="deleteMatchModal<?php echo $match['match_id']; ?>" tabindex="-1" aria-labelledby="deleteMatchModalLabel<?php echo $match['match_id']; ?>" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <!-- Delete Match Modal Content Goes Here -->
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
 
-        <?php
-        foreach ($matchesData as $competition => $matches) {
-        ?>
-            <div class="table-container">
-                <h2><?php echo htmlspecialchars($competition); ?></h2>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Home Team</th>
-                            <th>Away Team</th>
-                            <th>Date</th>
-                            <th>Score</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($matches as $match) { ?>
-                            <tr>
-                                <td><?php echo htmlspecialchars($match['team1_name']); ?></td>
-                                <td><?php echo htmlspecialchars($match['team2_name']); ?></td>
-                                <td><?php echo htmlspecialchars($match['match_date']); ?></td>
-                                <td><?php echo htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']); ?>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+        <!-- Add Match Modal for Each Competition -->
+        <div class="modal fade" id="addMatchModal<?php echo htmlspecialchars($competition); ?>" tabindex="-1" aria-labelledby="addMatchModalLabel<?php echo htmlspecialchars($competition); ?>" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- Add Match Modal Content Goes Here -->
+                </div>
             </div>
-        <?php
-        }
-        ?>
+        </div>
+    <?php
+    }
+    ?>
+</div>
 
-    </div>
+<?php include "view-footer.php"; ?>
 
-    <?php include "view-footer.php"; ?>
+<!-- JavaScript for Add, Edit, and Delete Modals -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
-    <!-- Bootstrap JavaScript and dependencies -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+<script>
+    // JavaScript code for handling add, edit, and delete modals
+    // ...
+</script>
