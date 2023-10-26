0c742896b9fc733b08556ba998814e6ae04c7969
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-26 00:36:15 -0500

    Update process-add-match.php

--- process-add-match.php
+++ process-add-match.php
@@ -1,28 +1,30 @@
 <?php
 require_once("util-db.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['actionType']) && $_POST['actionType'] == "Add") {
-    $team1Id = $_POST['team1'];
-    $team2Id = $_POST['team2'];
-    $matchDate = $_POST['matchDate'];
-    $scoreTeam1 = $_POST['scoreTeam1'];
-    $scoreTeam2 = $_POST['scoreTeam2'];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['actionType'])) {
+    if ($_POST['actionType'] == "Add") {
+        $team1Id = $_POST['team1'];
+        $team2Id = $_POST['team2'];
+        $matchDate = $_POST['matchDate'];
+        $scoreTeam1 = $_POST['scoreTeam1'];
+        $scoreTeam2 = $_POST['scoreTeam2'];
+        $competitionId = $_POST['competition']; // Add this line
 
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `matches` (`team1_id`, `team2_id`, `match_date`, `score_team1`, `score_team2`) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("iissi", $team1Id, $team2Id, $matchDate, $scoreTeam1, $scoreTeam2);
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
-
-        // Redirect back to the matches page after adding the match
-        header("Location: matches.php");
-        exit();
-    } catch (Exception $e) {
-        // Handle exceptions, display error message, etc.
-        // You can also redirect the user back to the form with an error message
-        echo "Failed to add match. Please try again.";
+        try {
+            $success = addMatch($team1Id, $team2Id, $matchDate, $scoreTeam1, $scoreTeam2, $competitionId);
+            if ($success) {
+                // Redirect back to the matches page after adding the match
+                header("Location: matches.php");
+                exit();
+            } else {
+                echo "Failed to add match. Please try again.";
+            }
+        } catch (Exception $e) {
+            // Handle exceptions, log errors, etc.
+            echo "Failed to add match. Please try again.";
+        }
+    } else {
+        // Handle other action types if needed
     }
 } else {
     // Invalid request, redirect the user to the home page or show an error message
