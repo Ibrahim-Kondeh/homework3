c147d510949fd90b9ded2a353ee1ff47c307b505
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-11 21:47:30 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -1,18 +1,29 @@
 <?php
+// model-players-stats.php
+
 function selectPlayers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("
-SELECT player_name, date_of_birth, nationality, position
-FROM player");
+        $stmt = $conn->prepare("SELECT nationality FROM player");
         $stmt->execute();
         $result = $stmt->get_result();
+        
+        $countryCounts = array();
+        while ($row = $result->fetch_assoc()) {
+            $nationality = $row['nationality'];
+            if (isset($countryCounts[$nationality])) {
+                $countryCounts[$nationality]++;
+            } else {
+                $countryCounts[$nationality] = 1;
+            }
+        }
+
         $conn->close();
-        return $result;
+
+        return $countryCounts;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
     }
 }
-
 ?>
