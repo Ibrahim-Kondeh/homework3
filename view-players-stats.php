508b96ae55257da8bb13c9c2e0faa737b09aab58
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-12 01:09:34 -0600

    Update view-players-stats.php

--- view-players-stats.php
+++ view-players-stats.php
@@ -205,3 +205,5 @@ const countryPieChart = new Chart(pieCtx, {
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
+</body>
+</html>
