add2657c06be491bd3990a2e539235f823f028d6
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-13 11:41:41 -0600

    Update view-players-stats.php

--- view-players-stats.php
+++ view-players-stats.php
@@ -188,7 +188,9 @@
     </script>
 
     <!-- Script for Cesium -->
-    <script>
+<script src="https://unpkg.com/cesium@1.84.0/Build/Cesium/Cesium.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
         // Initialize Cesium globe
         Cesium.Ion.defaultAccessToken = 'your-Cesium-Ion-token'; // Get your free Cesium Ion token: https://cesium.com/ion/signup/
 
@@ -231,7 +233,8 @@
                 }
             });
         });
-    </script>
+    });
+</script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
 </body>
