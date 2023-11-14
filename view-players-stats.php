7c4622efcad5ff1f57cac5ad5b705e5e84c86eb0
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-13 18:56:07 -0600

    Update view-players-stats.php

--- view-players-stats.php
+++ view-players-stats.php
@@ -7,10 +7,9 @@
     <title>Players</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css">
-    <script src="https://unpkg.com/globe.gl"></script> <!-- Add globe.gl script tag -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <style>
-        /* Custom CSS to style the players table */
         .player-table {
             width: 60%;
             border-collapse: collapse;
@@ -22,14 +21,13 @@
             padding: 8px;
             border: 1px solid #ddd;
             text-align: left;
-            font-size: 14px; /* Adjust font size as needed */
+            font-size: 14px;
         }
 
         .player-table th {
             background-color: #f2f2f2;
         }
 
-        /* Custom CSS for table row highlighting */
         .highlight-row:hover {
             background-color: #ff0000;
         }
@@ -38,7 +36,6 @@
             margin-top: 20px;
         }
 
-        /* Style for the chart containers */
         .chart-container {
             display: flex;
             justify-content: space-around;
@@ -47,11 +44,6 @@
 
         .chart {
             width: 45%;
-        }
-
-        #globe-container {
-            width: 100%;
-            height: 400px;
         }
     </style>
 </head>
@@ -68,30 +60,26 @@
             </thead>
             <tbody>
                 <?php
-                // Function to generate distinct colors
                 function generateColors($numColors)
                 {
                     $colors = [];
 
                     for ($i = 0; $i < $numColors; $i++) {
                         $hue = ($i * 360 / $numColors) % 360;
-                        $colors[] = "hsl($hue, 70%, 60%)"; // You can adjust saturation and lightness as needed
+                        $colors[] = "hsl($hue, 70%, 60%)";
                     }
 
                     return $colors;
                 }
 
-                // Get unique countries and their player counts
                 $uniqueCountriesData = selectPlayers();
-
-                // Generate distinct colors for each country
                 $countryColors = generateColors(count($uniqueCountriesData));
 
                 foreach ($uniqueCountriesData as $index => $row) {
                     $nationality = $row['nationality'];
                     $flagIcon = getFlagIcon($nationality);
                     $playerCount = $row['playerCount'];
-                    $color = $countryColors[$index]; // Assign a unique color to each country
+                    $color = $countryColors[$index];
                 ?>
                     <tr class="highlight-row">
                         <td><?php echo $nationality; ?></td>
@@ -104,53 +92,16 @@
         </table>
     </div>
 
-    <!-- Chart.js CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-    <!-- Chart containers -->
     <div class="chart-container">
-        <!-- Bar Chart -->
         <div class="chart">
             <canvas id="countryChart" width="400" height="200"></canvas>
         </div>
-
-        <!-- Pie Chart -->
         <div class="chart">
             <canvas id="countryPieChart" width="400" height="200"></canvas>
         </div>
     </div>
 
-    <div id="globe-container"></div>
-
-    <!-- Script for globe.gl -->
-    <script>
-        // Dynamic data for the globe (without duplicates)
-        const locations = <?php echo json_encode($uniqueCountriesData); ?>;
-
-        // Get the container div for the globe
-        const globeContainer = document.getElementById('globe-container');
-
-        if (globeContainer) {
-            // Initialize globe.gl
-            const globe = Globe({
-                container: '#globe-container',
-                globeImageUrl: 'https://unpkg.com/three-globe/example/img/earth-night.jpg', // Sample image URL
-                pointsData: locations.map(location => ({
-                    lat: location.latitude,
-                    lon: location.longitude,
-                    label: location.nationality,
-                    color: 'rgba(75, 192, 192, 0.7)', // Adjust color as needed
-                })),
-                pointLabel: 'label',
-                pointAltitude: 0.1,
-            });
-        } else {
-            console.error('Container not found.');
-        }
-    </script>
-
     <?php
-    // Dynamic data for charts (without duplicates)
     $labels = array_map(function ($row) {
         return $row['nationality'];
     }, $uniqueCountriesData);
@@ -159,16 +110,13 @@
     }, $uniqueCountriesData);
     ?>
     <script>
-        // Dynamic data for charts (without duplicates)
         const labels = <?php echo json_encode($labels); ?>;
         const counts = <?php echo json_encode($counts); ?>;
         const countryColors = <?php echo json_encode($countryColors); ?>;
 
-        // Get canvas elements and contexts
         const ctx = document.getElementById('countryChart').getContext('2d');
         const pieCtx = document.getElementById('countryPieChart').getContext('2d');
 
-        // Create bar chart
         const countryChart = new Chart(ctx, {
             type: 'bar',
             data: {
@@ -176,7 +124,7 @@
                 datasets: [{
                     label: 'Number of Countries Represented',
                     data: counts,
-                    backgroundColor: countryColors, // Use the generated colors
+                    backgroundColor: countryColors,
                     borderWidth: 1
                 }]
             },
@@ -189,14 +137,13 @@
             }
         });
 
-        // Create pie chart
         const countryPieChart = new Chart(pieCtx, {
             type: 'pie',
             data: {
                 labels: labels,
                 datasets: [{
                     data: counts,
-                    backgroundColor: countryColors, // Use the generated colors
+                    backgroundColor: countryColors,
                     borderWidth: 1
                 }]
             },
