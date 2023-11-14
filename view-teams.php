1083ca8beb52dcf9451b6ba9cbf688848d24086a
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-13 19:33:09 -0600

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -49,23 +49,67 @@
         <div class="row">
             <div class="col-md-9">
                 <h1>Teams</h1>
-                <?php
-                if (!empty($successMessage)) {
-                    echo '<script>
-                            Swal.fire({
-                                icon: "success",
-                                title: "Success!",
-                                text: "' . $successMessage . '"
-                            });
-                          </script>';
-                }
-                ?>
                 <table class="team-table">
-                    <!-- ... rest of your table code ... -->
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Team Name</th>
+                            <th>Coach</th>
+                            <th>Founded Year</th>
+                            <th>Home Stadium</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $rowNumber = 0;
+                        while ($team = $teams->fetch_assoc()) {
+                        ?>
+                        <tr class="highlight-row">
+                            <td><?php echo $team['team_id']; ?></td>
+                            <td><?php echo $team['team_name']; ?></td>
+                            <td><?php echo $team['coach_name']; ?></td>
+                            <td><?php echo $team['founded_year']; ?></td>
+                            <td><?php echo $team['home_stadium']; ?></td>
+                            <td><?php include "view-teams-editform.php"; ?></td>
+                            <td>
+                                <form method="post" action="">
+                                    <!-- Your delete form data here -->
+                                    <button type="submit" class="btn btn-danger btn-sm" name="deleteTeam"
+                                        onclick="return confirm('Are you sure you want to delete this team?');">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                            class="bi bi-trash3" viewBox="0 0 16 16">
+                                            <path
+                                                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                        </svg>
+                                        <?php echo "<script>showDeleteMessage('" . $team['team_name'] . "');</script>"; ?>
+                                    </button>
+                                </form>
+                            </td>
+
+                            <td>
+                                <form method="post" action="">
+                                    <!-- Your additional form data here -->
+                                </form>
+                            </td>
+                            <td>
+                                <a href="players-by-team.php?id=<?php echo $team['team_id']; ?>"
+                                    class="btn btn-secondary btn-sm">
+                                    <i class="bi bi-person"></i> Players
+                                </a>
+                            </td>
+                        </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
                 </table>
             </div>
             <div class="col-md-3">
-                <!-- ... your form code ... -->
+                <!-- Include the form within the modal -->
+                <?php include "view-teams-newform.php"; ?>
             </div>
         </div>
     </div>
