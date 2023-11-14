b9ccde3c5f1dc790f722b08dd5b1bd4b044b0429
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-13 19:09:54 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -41,12 +41,4 @@ if (!empty($successMessage)) {
 $teams = selectTeams();
 include "view-teams.php";
 include "view-footer.php";
-?><?php
-require_once("util-db.php");
-require_once("model-teams.php");
-$pageTitle = "Teams";
-include "view-header.php";
-$teams = selectTeams();
-include "view-teams.php";
-include "view-footer.php";
 ?>
