a54837e82d530fb0bfe0e0e51a1f8b844a9d3ed7
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-14 13:03:57 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -7,15 +7,36 @@ include "view-header.php";
 
 $successMessage = "";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['deleteTeam'])) {
-        $teamId = $_POST['teamId'];
-        if (deleteTeams($teamId)) {
-            $successMessage = "Team deleted successfully!";
-        } else {
-            $successMessage = "Failed to delete team. Please try again.";
-        }
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear'], $_POST['tStadium'])) {
+                $successMessage = "Team added successfully! 😊";
+            } else {
+                $successMessage = "Failed to add team. Please try again.";
+            }
+            break;
+
+        case "Edit":
+            if (updateTeams($_POST['tName'], $_POST['tcName'], $_POST['tFyear'], $_POST['tStadium'], $_POST['teamId'])) {
+                $successMessage = "Team edited successfully! 😊";
+            } else {
+                $successMessage = "Failed to edit team. Please try again.";
+            }
+            break;
+
+        case "Delete":
+            if (deleteTeams($_POST['teamId'])) {
+                $successMessage = "Team deleted successfully!";
+            } else {
+                $successMessage = "Failed to delete team. Please try again.";
+            }
+            break;
     }
+
+    // Redirect to clear the POST data and prevent message re-display on page refresh
+    header("Location: teams.php");
+    exit();
 }
 
 $teams = selectTeams();
