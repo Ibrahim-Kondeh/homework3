e7dbf38bdd04e75d74352ed74cf92002a1a7b011
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-14 12:32:58 -0600

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -64,7 +64,6 @@
                     </thead>
                     <tbody>
                         <?php
-                        $rowNumber = 0;
                         while ($team = $teams->fetch_assoc()) {
                         ?>
                         <tr class="highlight-row">
@@ -73,18 +72,17 @@
                             <td><?php echo $team['coach_name']; ?></td>
                             <td><?php echo $team['founded_year']; ?></td>
                             <td><?php echo $team['home_stadium']; ?></td>
-                            <td><?php include "view-teams-editform.php"; ?></td>
                             <td>
                                 <form method="post" action="">
                                     <input type="hidden" name="teamId" value="<?php echo $team['team_id']; ?>">
                                     <button type="submit" class="btn btn-danger btn-sm" name="deleteTeam"
                                         onclick="return confirm('Are you sure you want to delete this team?');">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                            class="bi bi-trash3" viewBox="0 0 16 16">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                            fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                                             <path
-                                                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                                                d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z">
+                                            </path>
                                         </svg>
-                                        <?php echo "<button type='button' class='d-none' id='deleteTeamButton' onclick='showDeleteMessage(\"" . $team['team_name'] . "\");'></button>"; ?>
                                     </button>
                                 </form>
                             </td>
@@ -116,18 +114,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ZOsT2UQzY3FN8LkFDrF4D72KlSb0P9ABqT1ggK5biQOp6iUAZjA8M2reF5FOSta0" crossorigin="anonymous"></script>
-
-    <!-- Add this script section at the end of the file -->
-    <script>
-        // Function to show delete message
-        function showDeleteMessage(teamName) {
-            Swal.fire({
-                title: "Team Deleted!",
-                text: "The team " + teamName + " has been deleted.",
-                icon: "success"
-            });
-        }
-    </script>
 </body>
 
 </html>
