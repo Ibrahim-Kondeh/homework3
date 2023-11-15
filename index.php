d3e2334a2ee456ce2f61ccda8271cc83f89730bc
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-11-15 11:54:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,43 +7,39 @@ include "view-header.php";
 
 <div class="highlights-container">
     <h2>Last Week Highlights</h2>
-    <div class="slideshow-container">
-        <!-- Add your image elements with alt text for accessibility -->
-        <div class="mySlides fade"><img src="https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F221104122416-arsenal-celebrate-card.jpg" alt="Highlight 2"></div>
-        <div class="mySlides fade"><img src="https://icibeyrouth.com/wp-content/uploads/2022/05/mbape.jpg" alt="Highlight 2"></div>
-        <div class="mySlides fade"><img src="https://i2-prod.mirror.co.uk/sport/article30851283.ece/ALTERNATES/s1200d/0_GettyImages-1655137419.jpg" alt="Highlight 1"></div>
-        <div class="mySlides fade"><img src="https://www.mpl.live/blog/wp-content/uploads/2021/09/man-city-1.jpg" alt="Highlight 2"></div>
-        <!-- Add more images as needed -->
+    <div class="glide">
+        <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+                <!-- Add your image elements with alt text for accessibility -->
+                <li class="glide__slide"><img src="https://media.gq-magazine.co.uk/photos/640b02e70a046f0156dd5365/16:9/w_2560%2Cc_limit/GettyImages-1247252693.jpg" alt="Highlight 1"></li>
+                <li class="glide__slide"><img src="https://www.mpl.live/blog/wp-content/uploads/2021/09/man-city-1.jpg" alt="Highlight 2"></li>
+                <!-- Add more images as needed -->
+                <li class="glide__slide"><img src="https://example.com/path/to/image3.jpg" alt="Highlight 3"></li>
+                <li class="glide__slide"><img src="https://example.com/path/to/image4.jpg" alt="Highlight 4"></li>
+                <!-- Add more images as needed -->
+            </ul>
+        </div>
     </div>
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+
 <script>
     document.addEventListener('DOMContentLoaded', () => {
-        let slideIndex = 0;
-
-        function showSlides() {
-            const slides = document.getElementsByClassName("mySlides");
-            
-            for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-
-            slideIndex++;
-
-            if (slideIndex > slides.length) {
-                slideIndex = 1;
-            }
-
-            slides[slideIndex - 1].style.display = "block";
-            setTimeout(showSlides, 5000); // Change slide every 5 seconds (adjust as needed)
-        }
-
-        showSlides();
+        // Initialize Glide for image slideshow
+        new Glide('.glide', {
+            type: 'carousel',
+            startAt: 0, // Start at the first slide
+            perView: 1, // Show one slide at a time
+            autoplay: 5000, // Change slide every 5 seconds (adjust as needed)
+            hoverpause: true, // Pause autoplay on hover
+            animationDuration: 800, // Animation duration in milliseconds
+        }).mount();
     });
 </script>
 
 <style>
-    /* Add your styles for highlights container and slideshow container as needed */
+    /* Add your styles for highlights container and Glide container as needed */
     .highlights-container {
         margin-top: 20px;
         max-width: 800px; /* Set the maximum width of the highlights container */
@@ -51,14 +47,11 @@ include "view-header.php";
         margin-right: auto;
     }
 
-    .slideshow-container {
-        max-width: 100%;
-        position: relative;
-        margin: auto;
+    .glide {
+        width: 100%;
     }
 
-    .mySlides {
-        display: none;
+    .glide__slides img {
         width: 100%; /* Make images fill the container */
         height: auto;
     }
