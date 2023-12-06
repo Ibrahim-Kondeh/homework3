b3ddac554c6e484d2fb29c676471fd8b57c3e3a7
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-06 10:41:37 -0600

    Create view-matches-editform.php

---
+++ view-matches-editform.php
@@ -0,0 +1,55 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editMatchModal">
+  Edit Match
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="editMatchModal" tabindex="-1" aria-labelledby="editMatchModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editMatchModalLabel">Edit Match</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <div class="mb-3">
+            <label for="competition" class="form-label">Competition</label>
+            <select class="form-select" id="competition_id" name="competition_id" required>
+              <!-- Populate this dropdown with competition names -->
+              <!-- Example: <option value="1">Premier League</option> -->
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="homeTeam" class="form-label">Home Team</label>
+            <select class="form-select" id="team1_id" name="team1_id" required>
+              <!-- Populate this dropdown with team names -->
+              <!-- Example: <option value="1">Team A</option> -->
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="awayTeam" class="form-label">Away Team</label>
+            <select class="form-select" id="team2_id" name="team2_id" required>
+              <!-- Populate this dropdown with team names -->
+              <!-- Example: <option value="2">Team B</option> -->
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="matchDate" class="form-label">Match Date</label>
+            <input type="date" class="form-control" id="match_date" name="match_date" required>
+          </div>
+          <div class="mb-3">
+            <label for="scoreTeam1" class="form-label">Score Team 1</label>
+            <input type="number" class="form-control" id="score_team1" name="score_team1" required>
+          </div>
+          <div class="mb-3">
+            <label for="scoreTeam2" class="form-label">Score Team 2</label>
+            <input type="number" class="form-control" id="score_team2" name="score_team2" required>
+          </div>
+          <input type="hidden" name="actionType" value="Edit">
+          <button type="submit" class="btn btn-primary">Update Match</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
