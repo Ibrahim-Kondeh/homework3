1d9657e0d429e357836d5a84d18a7ee8470920df
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-06 10:17:22 -0600

    Update model-matches.php

--- model-matches.php
+++ model-matches.php
@@ -1,9 +1,58 @@
 <?php
+// model-matches.php
 
+require_once("util-db.php");
+
+function insertMatches($competitionId, $team1Id, $team2Id, $matchDate, $score1, $score2) {
+    try {
+        $conn = get_db_connection();
+        // Implement the database insertion logic here
+        // Use prepared statements to prevent SQL injection
+        $stmt = $conn->prepare("INSERT INTO matches (competition_id, team1_id, team2_id, match_date, score_team1, score_team2) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("iiissi", $competitionId, $team1Id, $team2Id, $matchDate, $score1, $score2);
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful insertion
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+function updateMatches($matchId, $team1Id, $team2Id, $matchDate, $score1, $score2) {
+    try {
+        $conn = get_db_connection();
+        // Implement the database update logic here
+        $stmt = $conn->prepare("UPDATE matches SET team1_id=?, team2_id=?, match_date=?, score_team1=?, score_team2=? WHERE match_id=?");
+        $stmt->bind_param("iissii", $team1Id, $team2Id, $matchDate, $score1, $score2, $matchId);
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful update
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+function deleteMatches($matchId) {
+    try {
+        $conn = get_db_connection();
+        // Implement the database deletion logic here
+        $stmt = $conn->prepare("DELETE FROM matches WHERE match_id=?");
+        $stmt->bind_param("i", $matchId);
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful deletion
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
 
 function getAllMatchesGroupedByCompetition() {
     try {
         $conn = get_db_connection();
+        // Retrieve matches data from the database and structure it accordingly
         $query = "SELECT c.competition_name, m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
                   FROM matches m 
                   JOIN competition c ON m.competition_id = c.competition_id
