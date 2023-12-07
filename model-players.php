08aa6b4fa943c79a08b912b067acceae9a69bd4e
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 04:00:59 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -4,22 +4,23 @@ function selectPlayers() {
         if (!$conn) {
             throw new Exception("Failed to connect to the database.");
         }
-
-        $stmt = $conn->prepare("
-            SELECT player_id, player_name, date_of_birth, nationality, position
-            FROM player");
-
+        
+        $stmt = $conn->prepare("SELECT player_id, player_name, date_of_birth, nationality, position FROM player");
         if (!$stmt) {
             throw new Exception("Failed to prepare the query.");
         }
 
-        $stmt->execute();
+        $success = $stmt->execute();
+        if (!$success) {
+            throw new Exception("Failed to execute the query.");
+        }
+
         $result = $stmt->get_result();
         $conn->close();
+
         return $result;
     } catch (Exception $e) {
-        // Handle the exception, log it, or display an error message
-        error_log($e->getMessage());
+        error_log($e->getMessage()); // Log the error message
         return false; // Return false or handle the error as needed
     }
 }
