dc644c4b07e166aa012858d50d875d473b1fb2e2
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 03:52:26 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -3,8 +3,8 @@ function selectPlayers() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("
-SELECT player_name, date_of_birth, nationality, position
-FROM player");
+            SELECT player_id, player_name, date_of_birth, nationality, position
+            FROM players");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -15,4 +15,45 @@ FROM player");
     }
 }
 
+function insertPlayer($playerName, $dob, $nationality, $position) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO players (player_name, date_of_birth, nationality, position) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $playerName, $dob, $nationality, $position);
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful insertion
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+function updatePlayer($playerName, $dob, $nationality, $position, $playerId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE players SET player_name = ?, date_of_birth = ?, nationality = ?, position = ? WHERE player_id = ?");
+        $stmt->bind_param("ssssi", $playerName, $dob, $nationality, $position, $playerId);
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful update
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+function deletePlayer($playerId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM players WHERE player_id = ?");
+        $stmt->bind_param("i", $playerId);
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful deletion
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
 ?>
