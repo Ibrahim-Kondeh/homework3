b84f3aa0f7b84a5f15aa9a8dd8a83c1f2fbfd03f
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 03:55:21 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -1,59 +1,25 @@
-<?php
 function selectPlayers() {
     try {
         $conn = get_db_connection();
+        if (!$conn) {
+            throw new Exception("Failed to connect to the database.");
+        }
+
         $stmt = $conn->prepare("
             SELECT player_id, player_name, date_of_birth, nationality, position
             FROM players");
+
+        if (!$stmt) {
+            throw new Exception("Failed to prepare the query.");
+        }
+
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
     } catch (Exception $e) {
-        $conn->close();
-        throw $e;
+        // Handle the exception, log it, or display an error message
+        error_log($e->getMessage());
+        return false; // Return false or handle the error as needed
     }
 }
-
-function insertPlayer($playerName, $dob, $nationality, $position) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO players (player_name, date_of_birth, nationality, position) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $playerName, $dob, $nationality, $position);
-        $success = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        return $success; // Return success to indicate successful insertion
-    } catch (Exception $e) {
-        throw $e;
-    }
-}
-
-function updatePlayer($playerName, $dob, $nationality, $position, $playerId) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE players SET player_name = ?, date_of_birth = ?, nationality = ?, position = ? WHERE player_id = ?");
-        $stmt->bind_param("ssssi", $playerName, $dob, $nationality, $position, $playerId);
-        $success = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        return $success; // Return success to indicate successful update
-    } catch (Exception $e) {
-        throw $e;
-    }
-}
-
-function deletePlayer($playerId) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("DELETE FROM players WHERE player_id = ?");
-        $stmt->bind_param("i", $playerId);
-        $success = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        return $success; // Return success to indicate successful deletion
-    } catch (Exception $e) {
-        throw $e;
-    }
-}
-?>
