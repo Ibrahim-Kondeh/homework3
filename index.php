70d06c9fbc3e021db167b79b84e1958b59705aba
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 02:14:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,41 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
+<h1>RAISE THE GAME</h1>
 <h1>HomeWork 6</h1>
 
-<div class="highlights-container">
-    <h2>Last Week Highlights</h2>
+<div class="container">
+    <div class="row">
+        <!-- Video Section -->
+        <div class="col-md-6">
+            <h2>Last Week Highlights</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- Video 1 -->
+                    <div class="card">
+                        <video src="..." class="object-fit-contain" autoplay></video>
+                        <div class="card-body">
+                            <h5 class="card-title">Highlight 1</h5>
+                            <!-- Add video description here -->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <!-- Video 2 -->
+                    <div class="card">
+                        <video src="..." class="object-fit-cover" autoplay></video>
+                        <div class="card-body">
+                            <h5 class="card-title">Highlight 2</h5>
+                            <!-- Add video description here -->
+                        </div>
+                    </div>
+                    <!-- Add more video cards as needed -->
+                </div>
+            </div>
+        </div>
+
+           <div class="col-md-6">
+            <h2>Slideshow</h2>
     <div class="slideshow-container">
         <!-- Add your image elements with alt text for accessibility -->
         <div class="mySlides fade"><img src="https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F221104122416-arsenal-celebrate-card.jpg" alt="Highlight 2"></div>
@@ -15,6 +46,8 @@ include "view-header.php";
         <div class="mySlides fade"><img src="https://i2-prod.mirror.co.uk/sport/article30851283.ece/ALTERNATES/s1200d/0_GettyImages-1655137419.jpg" alt="Highlight 1"></div>
         <div class="mySlides fade"><img src="https://www.mpl.live/blog/wp-content/uploads/2021/09/man-city-1.jpg" alt="Highlight 2"></div>
         <!-- Add more images as needed -->
+   </div>
+        </div>
     </div>
 </div>
 
