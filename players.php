e433119aae30d535410bdf8c064b2ad7b7984123
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-08 02:12:03 -0600

    Update players.php

--- players.php
+++ players.php
@@ -12,7 +12,7 @@ $successMessage = "";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            if (insertPlayer($_POST['pName'], $_POST['pDob'], $_POST['pNationality'], $_POST['pPosition'], $_POST['teamId'])) {
+            if (insertPlayer($_POST['pName'], $_POST['pDob'], $_POST['pNationality'], $_POST['position'], $_POST['teamId'])) {
                 $successMessage = "Player added successfully! 😊";
             } else {
                 $successMessage = "Failed to add Player. Please try again.";
