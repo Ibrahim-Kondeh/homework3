189cd8c326ab25c54cfca24d031e34dead9582a6
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-08 02:58:56 -0600

    Update view-players-newform.php

--- view-players-newform.php
+++ view-players-newform.php
@@ -16,42 +16,35 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                
-              <form method="post" action="">
-                      <div class="mb-3">
+                <form method="post" action="players.php">
+                    <div class="mb-3">
                         <label for="pName" class="form-label">Player Name</label>
                         <input type="text" class="form-control" id="pName" name="pName">
-                      </div>
-                  
-                      <div class="mb-3">
+                    </div>
+                    <div class="mb-3">
                         <label for="pDob" class="form-label">Date of Birth</label>
                         <input type="date" class="form-control" id="pDob" name="pDob">
                     </div>
-                  <div class="mb-3">
+                    <div class="mb-3">
                         <label for="pNationality" class="form-label">Nationality</label>
                         <input type="text" class="form-control" id="pNationality" name="pNationality">
                     </div>
-                   <div class="mb-3">
-                        <label for="pPosition" class="form-label">Position</label>
+                    <div class="mb-3">
+                        <label for="position" class="form-label">Position</label>
                         <input type="text" class="form-control" id="position" name="position">
                     </div>
-                      <div class="mb-3">
-                        <label for="teamId" class="form-label">Team</label>
-                        <input type="text" class="form-control" id="position" name="teamId">
-                    </div>
                       <!-- Team selection dropdown -->
-                    <div class="mb-3">
+                  <div class="mb-3">
                         <label for="teamName" class="form-label">Team</label>
                         <select class="form-select" id="teamName" name="teamName">
                             <?php
                             $teams = selectTeams();
                             foreach ($teams as $team) {
-                                echo "<option value='" . $team['team_name'] . "'>" . $team['team_name'] . "</option>";
+                                echo "<option value='" . $team['team_id'] . "'>" . $team['team_name'] . "</option>";
                             }
                             ?>
                         </select>
                     </div>
-
                     <input type="hidden" name="actionType" value="Add">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
