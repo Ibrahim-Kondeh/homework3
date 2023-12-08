6666ac9334677f755a602896d44f3be512b57a0a
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 22:02:30 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -1,42 +1,43 @@
+<?php
 function selectPlayers() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT player_id, player_name, date_of_birth, nationality, position FROM player");
         $stmt->execute();
         $result = $stmt->get_result();
-        $stmt->close();
         $conn->close();
         return $result;
     } catch (Exception $e) {
-        error_log($e->getMessage()); // Log the error message
-        return false; // Return false or handle the error as needed
-    }
-}
-
-function insertPlayer($pName, $pDob, $pNationality, $pPosition) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `player` (`player_name`, `date_of_birth`, `nationality`, `position`) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $pName, $pDob, $pNationality, $pPosition);
-        $success = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        return $success; // Return success to indicate successful insertion
-    } catch (Exception $e) {
+         $conn->close();
         throw $e;
     }
 }
 
-function updatePlayer($pName, $pDob, $pNationality, $pPosition, $playerId) {
+
+function insertPlayer($pName, $pPosition, $pDob, $pNationality,) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `player` SET `player_name` = ?, `date_of_birth` = ?, `nationality` = ?, `position` = ? WHERE `player_id` = ?");
-        $stmt->bind_param("ssssi", $pName, $pDob, $pNationality, $pPosition, $playerId);
+        $stmt = $conn->prepare("INSERT INTO `player` (`player_name`, `position`, `date_of_birth`, `nationality`) VALUES (?, ?, ?, ?,)");
+        $stmt->bind_param("ssss", $pName, $pPosition, $pDob, $pNationality);
         $success = $stmt->execute();
-        $stmt->close();
         $conn->close();
-        return $success; // Return success to indicate successful update
+        return $success; // Return success to indicate successful insertion
     } catch (Exception $e) {
+         $conn->close();
+        throw $e;
+    }
+}
+
+function UpdatePlayer($pName, $pPosition, $pDob, $pNationality, $playerId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `player` set `player_name` = ?,  `position` = ?, `date_of_birth` = ?, `nationality` = ?, WHERE `player_id` = ?");
+        $stmt->bind_param("ssssi", $pName, $pPosition, $pDob, $pNationality, $playerId);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success; // Return success to indicate successful insertion
+    } catch (Exception $e) {
+         $conn->close();
         throw $e;
     }
 }
@@ -44,13 +45,17 @@ function updatePlayer($pName, $pDob, $pNationality, $pPosition, $playerId) {
 function deletePlayer($playerId) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("DELETE FROM `player` WHERE `player_id` = ?");
+        $stmt = $conn->prepare("delete from player where course_id=? ");
         $stmt->bind_param("i", $playerId);
         $success = $stmt->execute();
-        $stmt->close();
         $conn->close();
-        return $success; // Return success to indicate successful deletion
+        return $success; // Return success to indicate successful insertion
     } catch (Exception $e) {
+         $conn->close();
         throw $e;
     }
 }
+
+?>
+
+
