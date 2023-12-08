a4941ff6bf7c69bb7509f5f8b03f3026f52b8624
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 23:31:23 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -2,7 +2,10 @@
 function selectPlayers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_name, date_of_birth, nationality, position, team_id from player");
+        $stmt = $conn->prepare("SELECT p.player_name, p.date_of_birth, p.nationality, p.position, t.team_name
+                                FROM player AS p
+                                JOIN teams AS t ON p.team_id = t.team_id
+                                WHERE t.team_id = ?");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
