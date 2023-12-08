26a933fef91659072fb2181248d77c57155348be
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-07 20:40:41 -0600

    Update players.php

--- players.php
+++ players.php
@@ -4,8 +4,46 @@ require_once("model-players.php");
 
 $pageTitle = "Players";
 include "view-header.php";
+
+$successMessage = "";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertPlayers($_POST['pName'], $_POST['pDob'], $_POST['pNationality'], $_POST['pPosition'])) {
+                $successMessage = "Team added successfully! 😊";
+            } else {
+                $successMessage = "Failed to add team. Please try again.";
+            }
+            break;
+        case "Edit":
+            if (updatePlayers($_POST['pName'], $_POST['pDob'], $_POST['pNationality'], $_POST['pPosition'])) {
+                $successMessage = "Team edited successfully! 😊";
+            } else {
+                $successMessage = "Failed to edit team. Please try again.";
+            }
+            break;
+        case "Delete":
+            if (deletePlayers($_POST['playerId'])) {
+                $successMessage = "Team deleted successfully!";
+            } else {
+                $successMessage = "Failed to delete team. Please try again.";
+            }
+            break;
+    }
+}
+
+if (!empty($successMessage)) {
+    echo '<div class="alert alert-success text-center" role="alert">' . $successMessage . '</div>';
+}
+
+
+
+
+
+
 $players = selectPlayers();
-$player = selectPlayers();
+
 include "view-players.php";
 include "view-footer.php";
 ?>
