fdee6662710c830073a100f7d30b068dec914ed3
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-09 03:02:39 -0600

    Create team-input-list.php

---
+++ team-input-list.php
@@ -0,0 +1,13 @@
+<select class="form-select" id="teamId" name="teamId">
+<?php
+while ($teamItem = $teams->fetch_assoc()) {
+  $selText = "";
+  if ($selectedTeam == $teamItem['team_id']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $teamItem['team_id']; ?>"<?=$selText?>><?php echo $teamItem['teamName']; ?></option>
+<?php
+}
+?>
+</select>
