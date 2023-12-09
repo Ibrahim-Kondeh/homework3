096ab4b3817497f1ffcb9740b458bf5f7d1382be
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-09 00:49:30 -0600

    Update view-players-editform.php

--- view-players-editform.php
+++ view-players-editform.php
@@ -31,9 +31,7 @@
                         <label for="pPosition<?php echo $player['player_id'];?>" class="form-label">Position</label>
                         <input type="text" class="form-control" id="pPosition<?php echo $player['player_id'];?>" name="pPosition" value="<?php echo $player['position'];?>">
                         <?php
-$teams= selectTeamsForInput();
-$selectedTeam = $player['team_id'];
-?>
+
                     </div>
                     <input type="hidden" name="playerId" value="<?php echo $player['player_id'];?>">
                     <input type="hidden" name="actionType" value="Edit">
