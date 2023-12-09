daa2a942b6ec38eed195b21464bd95b1174f7cdb
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-08 23:38:46 -0600

    Update view-players-newform.php

--- view-players-newform.php
+++ view-players-newform.php
@@ -1,3 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-add" data-bs-toggle="modal" data-bs-target="#newPlayerModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+    </svg>
+</button>
+
+
 <!-- Modal -->
 <div class="modal fade" id="newPlayerModal" tabindex="-1" aria-labelledby="newPlayerModalLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -29,7 +47,7 @@
                         <label for="teamName" class="form-label">Team</label>
                         <select class="form-select" id="teamName" name="teamName">
                             <?php
-                            $teams = selectTeams();
+                            $teams = selectTeamsForInput();
                             foreach ($teams as $team) {
                                 echo "<option value='" . $team['team_id'] . "'>" . $team['team_name'] . "</option>";
                             }
