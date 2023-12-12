dca07ec23b73e337cb5104839806f0f1d2aa6d54
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-12 14:08:16 -0600

    Update competitions.php

--- competitions.php
+++ competitions.php
@@ -5,6 +5,35 @@ require_once("model-competitions.php");
 $pageTitle = "Competitions";
 include "view-header.php";
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertCompetition($_POST['cName'], $_POST['cStartDate'], $_POST['cEndDate'])) {
+        echo '<div class="alert alert-success" role="alert">Competition added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateCompetition($_POST['cName'], $_POST['cStartDate'], $_POST['cEndDate'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Competition edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteCompetition($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Course deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+
+
+
 $competitions = getAllCompetitions();
 
 include "view-competitions.php";
