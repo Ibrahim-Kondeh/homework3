998d7c28db9c89d3673a28ab9cf92cd9eb1ead13
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-12 05:09:02 -0600

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -43,39 +43,21 @@
                         <td><?php echo $player['position']; ?></td>
                         <td> <?php include "view-players-editform.php"; ?></td>
                         
-                        <td>
-                          <form method="post" action="">
-                            <input type="hidden" name="player_id" value="<?php echo $player['player_id']; ?>">
-                            <input type="hidden" name="actionType" value="Delete">
-                            <button type="submit" class="btn btn-primary" onclick="return confirm('Do you really want to delete?');">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                              </svg>
+                       <td>
+    <form method="post" action="">
+        <input type="hidden" name="player_id" value="<?php echo $player['player_id']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Do you really want to delete?');">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+            </svg>
+        </button>
+    </form>
+</td>
 
-
-                              <form method="post" action="">
-                <!-- ... Other input fields ... -->
-               <div class="mb-3">
-        <label for="teamName" class="form-label">Team</label>
-        <select class="form-select" id="teamName" name="teamName">
-            <?php
-            $teams = selectTeamsForInput();
-            while ($team = $teams->fetch_assoc()) {
-                echo '<option value="' . $team['team_id'] . '">' . $team['team_name'] . '</option>';
-            }
-            ?>
-        </select>
-    </div>
-    <!-- ... Submit button ... -->
-</form>
-
-                              
-                            </button>
-                          </form>
-                        </td>
                  
-                      </tr>
+                        </tr>
                       <?php
                       }
                       ?>
