846798df531d0e09c4b9e09df6f90db40e872987
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-12 15:30:20 -0600

    Update model-competitions.php

--- model-competitions.php
+++ model-competitions.php
@@ -4,7 +4,7 @@ require_once("util-db.php");
 function getAllCompetitions() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT competition_name, satart_date, end_date FROM competition");
+        $stmt = $conn->prepare("SELECT competition_id, competition_name, satart_date, end_date FROM competition");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
