06cc954879476dbe138316c0c427dcb12d744839
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-12 14:00:55 -0600

    Update model-competitions.php

--- model-competitions.php
+++ model-competitions.php
@@ -14,4 +14,48 @@ function getAllCompetitions() {
         throw $e;
     }
 }
+
+function insertCompetition($cName, $cStartDate, $cEndDate) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `competitions` (`competition_name`, `start_date`, `end_date`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cName, $cStartDate, $cEndDate);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCompetition($cName, $cStartDate, $cEndDate, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `competitions` set `competition_name` = ?, `start_date` = ?, `end_date` = ? where competition_id = ?");
+        $stmt->bind_param("sssi", $cName, $cStartDate, $cEndDate, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCompetition($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from competitions where competition_id=?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
