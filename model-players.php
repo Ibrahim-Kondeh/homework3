9c5e6e51db1b1396360aee4a43cfa8890ce5d5ef
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-12 04:56:18 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -29,11 +29,11 @@ function selectTeamsForInput() {
 }
 
 
-function insertPlayer($pName, $pPosition, $pDob, $pNationality, $teamId) {
+function insertPlayer($pName, $pDob, $pNationality, $pPosition,  $teamId) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `player` (`player_name`, `position`, `date_of_birth`, `nationality`, `team_id`) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssi", $pName, $pPosition, $pDob, $pNationality, $teamId);
+        $stmt = $conn->prepare("INSERT INTO `player` (`player_name`,  `date_of_birth`, `nationality`, `position`, `team_id`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssi", ($pName, $pDob, $pNationality, $pPosition,  $teamId);
         $success = $stmt->execute();
         $conn->close();
         return $success; // Return success to indicate successful insertion
@@ -46,7 +46,7 @@ function insertPlayer($pName, $pPosition, $pDob, $pNationality, $teamId) {
 function updatePlayer($pName, $pPosition, $pDob, $pNationality, $teamId, $playerId) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `player` SET `player_name` = ?, `position` = ?, `date_of_birth` = ?, `nationality` = ?, `team_id` = ? WHERE `player_id` = ?");
+        $stmt = $conn->prepare("UPDATE `player` SET `player_name` = ?, `date_of_birth` = ?, `nationality` = ?, `position` = ?,`team_id` = ? WHERE `player_id` = ?");
         $stmt->bind_param("ssssii", $pName, $pPosition, $pDob, $pNationality, $teamId, $playerId);
         $success = $stmt->execute();
         $conn->close();
