dce6c693e7a43acacd5126df1c594eb5b2d71227
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-12 03:41:00 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -43,7 +43,7 @@ function insertPlayer($pName, $pPosition, $pDob, $pNationality, $teamId) {
     }
 }
 
-function UpdatePlayer($pName, $pPosition, $pDob, $pNationality, $playerId) {
+function updatePlayer($pName, $pPosition, $pDob, $pNationality, $playerId) {
     try {
         $conn = get_db_connection();
     $stmt = $conn->prepare("UPDATE `player` SET `player_name` = ?, `position` = ?, `date_of_birth` = ?, `nationality` = ? WHERE `player_id` = ?");
