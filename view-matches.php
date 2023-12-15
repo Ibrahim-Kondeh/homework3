53ba0517ed81e3f3f30d26ba68b800d854cb6ef0
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-15 00:48:01 -0600

    Update view-matches.php

--- view-matches.php
+++ view-matches.php
@@ -74,19 +74,7 @@ include "view-matches-newform.php"
                             <td><?php echo htmlspecialchars($match['team2_name']); ?></td>
                             <td><?php echo htmlspecialchars($match['match_date']); ?></td>
                             <td><?php echo htmlspecialchars($match['score_team1']) . " : " . htmlspecialchars($match['score_team2']); ?></td>
-                            <td>
-    <?php foreach ($matches as $singleMatch) { ?>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editMatchModal<?php echo $singleMatch['match_id']; ?>">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-            </svg>
-        </button>
-        <div class="modal fade" id="editMatchModal<?php echo $singleMatch['match_id']; ?>" tabindex="-1" aria-labelledby="editMatchModalLabel<?php echo $singleMatch['match_id']; ?>" aria-hidden="true">
-            <!-- ... (modal code) ... -->
-        </div>
-    <?php } ?>
-</td>
+                           <td><?php include "view-matches-editform.php";?></td>
 
                             <td>
                                 <form method="post" action="">
