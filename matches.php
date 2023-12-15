36da6b571065c04358ae2bb7b2ef335a199be15b
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-15 03:39:11 -0600

    Update matches.php

--- matches.php
+++ matches.php
@@ -7,7 +7,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertMatches($_POST['competitionId'],$_POST['team1Id'], $_POST['team2Id'], $_POST['matchDate'],  $_POST['score1'],  $_POST['score2'] )) {
+      if (insertMatches($_POST['teamId'],$_POST['team1Id'], $_POST['team2Id'], $_POST['matchDate'],  $_POST['score1'],  $_POST['score2'] )) {
         echo '<div class="alert alert-success" role="alert">Match added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
