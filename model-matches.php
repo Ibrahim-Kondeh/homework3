d953527e6b559b024e45162843714c1993ff0f4a
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-12-15 06:10:54 -0600

    Update model-matches.php

--- model-matches.php
+++ model-matches.php
@@ -1,9 +1,12 @@
 <?php
+// model-matches.php
+
 require_once("util-db.php");
 
 function getAllMatchesGroupedByCompetition() {
     try {
         $conn = get_db_connection();
+        // Retrieve matches data from the database and structure it accordingly
         $query = "SELECT c.competition_name, m.match_id, t1.team_name AS team1_name, t2.team_name AS team2_name, m.match_date, m.score_team1, m.score_team2 
                   FROM matches m 
                   JOIN competition c ON m.competition_id = c.competition_id
@@ -24,45 +27,78 @@ function getAllMatchesGroupedByCompetition() {
     }
 }
 
-function addMatch($team1Id, $team2Id, $matchDate, $scoreTeam1, $scoreTeam2, $competitionId) {
+function getAllCompetitions() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO matches (team1_id, team2_id, match_date, score_team1, score_team2, competition_id) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("iissii", $team1Id, $team2Id, $matchDate, $scoreTeam1, $scoreTeam2, $competitionId);
+        $stmt = $conn->prepare("SELECT competition_id, competition_name, satart_date, end_date FROM competition");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectTeamsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name FROM `teams` order by team_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertMatches($competitionId, $team1Id, $team2Id, $matchDate, $score1, $score2) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO matches (competition_id, team1_id, team2_id, match_date, score_team1, score_team2) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("iiissi", $competitionId, $team1Id, $team2Id, $matchDate, $score1, $score2);
         $success = $stmt->execute();
         $stmt->close();
         $conn->close();
-        return $success;
+        return $success; // Return success to indicate successful insertion
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+function updateMatches($matchId, $team1Id, $team2Id, $matchDate, $score1, $score2) {
+
+    try {
+        $conn = get_db_connection();
+        // Implement the database update logic here
+        $stmt = $conn->prepare("UPDATE matches SET team1_id=?, team2_id=?, match_date=?, score_team1=?, score_team2=? WHERE match_id=?");
+      $stmt->bind_param("iissii", $team1Id, $team2Id, $matchDate, $score1, $score2, $matchId);
+
+        $success = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        return $success; // Return success to indicate successful update
     } catch (Exception $e) {
         throw $e;
     }
 }
 
-function editMatch($matchId, $team1Id, $team2Id, $matchDate, $scoreTeam1, $scoreTeam2, $competitionId) {
+function deleteMatches($matchId) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE matches SET team1_id = ?, team2_id = ?, match_date = ?, score_team1 = ?, score_team2 = ?, competition_id = ? WHERE match_id = ?");
-        $stmt->bind_param("iissiii", $team1Id, $team2Id, $matchDate, $scoreTeam1, $scoreTeam2, $competitionId, $matchId);
-        $success = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        throw $e;
-    }
-}
-
-function deleteMatch($matchId) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("DELETE FROM matches WHERE match_id = ?");
+        // Implement the database deletion logic here
+        $stmt = $conn->prepare("DELETE FROM matches WHERE match_id=?");
         $stmt->bind_param("i", $matchId);
         $success = $stmt->execute();
         $stmt->close();
         $conn->close();
-        return $success;
+        return $success; // Return success to indicate successful deletion
     } catch (Exception $e) {
         throw $e;
     }
 }
+
+
 ?>
